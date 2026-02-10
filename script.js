fcc4fb966f145cc0692e2123124fb16b4ebcadef
taxintian-cloud <taxintian@gmail.com>
2026-02-10 13:56:27 +0900

    Change toTopBtn display style to 'flex'

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// to-topボタンの表示/非表示
 const toTopBtn = document.getElementById('to-top');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) { // 300px以上スクロールしたら表示
-        toTopBtn.style.display = 'block';
+    if (window.scrollY > 300) {
+        toTopBtn.style.display = 'flex';
     } else {
         toTopBtn.style.display = 'none';
     }
 });
 
-// クリックでトップにスクロール
 toTopBtn.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
